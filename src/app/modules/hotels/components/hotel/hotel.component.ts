a1d8b766fec5ae4b19e3aeb8560adbf9aed04d87
Rachid Benkitou <rachidbenkitou2021@gmail.com>
2024-01-01 00:45:29 +0100

    new admin dashboard using ootstrap

--- src/app/modules/hotels/components/hotel/hotel.component.ts
+++ src/app/modules/hotels/components/hotel/hotel.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-hotel',
   templateUrl: './hotel.component.html',
-  styleUrls: ['./hotel.component.css']
+  styleUrls: ['./hotel.component.scss']
 })
 export class HotelComponent {
 
--- src/app/shared/components/sidebar/sidebar.component.ts
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -1,4 +1,4 @@
-import {Component, signal} from '@angular/core';
+import {Component, OnInit, signal} from '@angular/core';
 export type MenuItem= {
   icon: string;
   label: string;
@@ -9,8 +9,16 @@ export type MenuItem= {
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.scss']
 })
-export class SidebarComponent {
+export class SidebarComponent implements OnInit{
+  ngOnInit(): void {
+  }
 
+  //Sidebar toggle show hide function
+  status = false;
+  addToggle()
+  {
+    this.status = !this.status;
+  }
   menuItems= signal<MenuItem[]>([
     {
       icon: 'dashboard',
