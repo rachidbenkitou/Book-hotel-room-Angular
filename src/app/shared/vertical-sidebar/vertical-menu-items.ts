b66714db2e9d24b505cdcf2f3a0e42809ac97ee2
Rachid Benkitou <rachidbenkitou2021@gmail.com>
2024-02-20 03:04:30 +0100

    make p-table small, make sort-icon small, change logo

--- src/app/shared/vertical-sidebar/vertical-menu-items.ts
+++ src/app/shared/vertical-sidebar/vertical-menu-items.ts
@@ -26,30 +26,6 @@ export const ROUTES: RouteInfo[] = [
     submenu: []
   },
   {
-    path: '/categories',
-    title: 'Categories',
-    icon: 'codepen',
-    class: '',
-    extralink: false,
-    submenu: []
-  },
-  {
-    path: '/products',
-    title: 'Products',
-    icon: 'archive',
-    class: '',
-    extralink: false,
-    submenu: []
-  },
-  {
-    path: '/clientOrders',
-    title: 'Orders',
-    icon: 'codesandbox',
-    class: '',
-    extralink: false,
-    submenu: []
-  },
-  {
     path: '/clients',
     title: 'Clients',
     icon: 'user',
@@ -60,7 +36,7 @@ export const ROUTES: RouteInfo[] = [
 
   {
     path: '/WimNotifymodel',
-    title: 'm.setting',
+    title: 'Settings',
     icon: 'settings',
     class: 'has-arrow',
     extralink: false,
@@ -68,7 +44,7 @@ export const ROUTES: RouteInfo[] = [
     submenu: [
       {
         path: '/WimNotify/model',
-        title: 'm.model',
+        title: 'Model',
         icon: 'mdi mdi-layers',
         class: '',
         extralink: false,
